b66b884aff1553c88ef4bde36876c77521b7c51c
fdcir3tt <fd.cirett@gmail.com>
2025-11-20 12:52:12 -0700

    Se agregó filtro de categorías de producto , gráficas clave y los kpis que se utilizaran para la primera versión

--- app.py
+++ app.py
@@ -40,16 +40,20 @@ st.markdown('<div class="main-header"> Inventario CT International</div>', unsaf
 data = process_data ()
 categorias = pd.read_csv("data/categorias.csv")
 categorias = list(categorias["nombre"])
-
+top_category = gr.top_n(data,type="categoria",n=1)
 # -----------------------------------------------------------
 # FILTROS
 # -----------------------------------------------------------
+
 st.sidebar.header("Filtros")
+selected_categories = st.sidebar.multiselect("Categorías", data["category"].unique(),default=top_category)
+is_category = data["category"].isin(selected_categories)
+
 level = st.sidebar.radio("Nivel de Análisis",["Global","Sucursal"])
-product = st.sidebar.selectbox("Producto", data["productId"].unique())
+product = st.sidebar.selectbox("Producto", data[is_category]["productId"].unique())
 fecha_inicio = st.sidebar.date_input("Inicio", datetime.date(2025, 10, 1))
 fecha_fin = st.sidebar.date_input("Fin", datetime.date(2025, 11, 1))
-outliers= st.sidebar.radio("Análisis con ventas anomalas", ["Sí","No"])
+outliers= st.sidebar.radio("Análisis con ventas anomalas incluídas", ["No","Sí"])
 
 if level=="Sucursal":
     branch = st.sidebar.selectbox("Sucursal", data["sucursal"].unique())
@@ -70,15 +74,22 @@ if outliers=="Sí":
 else:
     data = gr.remove_outliers(data,"sales_day")
 # -----------------------------------------------------------
-gr.stock_v_sales(data,product)
-gr.sales_hist(data["sales_day"])
-gr.sales_heat_map(data,product)
-gr.profits_sales_bar(data, categorias[20:25])
+gr.stock_v_sales(data,product,start_date=fecha_inicio,end_date=fecha_fin)
+gr.sales_hist(data[["sales_day","fecha"]],start_date=fecha_inicio,end_date=fecha_fin)
+gr.sales_heat_map(data,product,start_date=fecha_inicio,end_date=fecha_fin)
+gr.abc_bar_chart(data,fecha_inicio,fecha_fin)
+gr.stockCov_vs_salesVel(data[is_category],start_date=fecha_inicio,end_date=fecha_fin)
 
 # -----------------------------------------------------------
 # VISUALIZACIÓN EN STREAMLIT
 # -----------------------------------------------------------
-st.image("plots/almacen_ventas.png", caption="Stock y Ventas - 2025", use_container_width=True)
+col1, col2 = st.columns(2)
+with col1:
+    st.image("plots/almacen_ventas.png", caption="Stock y Ventas - 2025", use_container_width=True)
+with col2:
+    st.image("plots/almacen_v_rapidez_ventas.png", caption="Stock Cover vs Rápidez de Ventas", use_container_width=True)
+
+
 
 # Info del producto
 
@@ -111,25 +122,22 @@ with col2:
 
 total_profit = data[is_product]["profit"].sum()
 total_sales = data[is_product]["sales_day"].sum()
-inventory_t_ratio = 0
+inventory_t_ratio =0
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.markdown(f'<div class="metric-card"><h3>Ganancia Total</h3><h2>${total_profit}</h2><p>+20% mes a mes</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card"><h3>Ganancia Total</h3><h2>${total_profit}</h2><p>ritmo</p></div>', unsafe_allow_html=True)
 with col2:
-    st.markdown(f'<div class="metric-card"><h3>Ventas Total</h3><h2>2,{total_sales}</h2><p>+12% mes a mes</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card"><h3>Ventas Total</h3><h2>2,{total_sales}</h2><p>ritmo</p></div>', unsafe_allow_html=True)
 with col3:
-    st.markdown(f'<div class="metric-card"><h3>Cociente Rotación de Inventario</h3><h2>{inventory_t_ratio}</h2><p>+1.2% mes a mes</p></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="metric-card"><h3>Cociente de Inventario</h3><h2>{inventory_t_ratio}</h2><p>ritmo</p></div>', unsafe_allow_html=True)
 
-# Top/Bottom 5
-top_n=gr.top_n(data)
-bot_n=gr.top_n(data,n=-5)
 
-st.markdown("####  Top 5 / Bottom 5")
-tab1, tab2 = st.tabs(["Top 5", "Bottom 5"])
-with tab1:
-    st.table(top_n)
-with tab2:
-    st.table(bot_n)
+
 
 # Gráfica de ventas y ganancia por categoría
-st.image("plots/bar_ventas_ganancia.png", caption="Ventas y Ganancia por Categoría", use_container_width=True)
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image("plots/abc_chart.png", caption="Ventas Totales por Producto", use_container_width=True)
+with col2:
+    st.image("plots/abc_chart.png", caption="Ventas Totales por Producto", use_container_width=True)
