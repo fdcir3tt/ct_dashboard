f376b7e443425aeed44854476199b7ed0344e183
fdcir3tt <fd.cirett@gmail.com>
2025-12-16 12:36:48 -0700

    Script prototipo de ingesta de datos de tabla existenciaHistorial

--- existence_schema.md
+++ existence_schema.md
@@ -1,14 +1,17 @@
-# Esquema de colección : tbl_existenciaHistorial
+# Esquema de colección : tbl_existenciasHistorial
 
 
 
 
 
-| **Nombre**                            | **Descripción**                                                    | **Requerido**         | **Formato de variable** | **Fuente**                  |
-| ------------------------------------- | ------------------------------------------------------------------ | -------------------- | ----------------------- | --------------------------- |
-| `_id`                            | Identificador único del documento                                      | Sí          | ObjectId          | CT_API_Publica / tbl_existenciaHistorial |
-| `productId`                            | Código identificador de producto                                       | Sí          | Texto / String          | CT_API_Publica / tbl_existencia |
-| `active`                            |  Describe si el producto esta actualmente activo                                       | Sí          | Booleano          | CT_API_Publica / tbl_existencia |
-| `units_stored`                            |  Cuántas unidades existen por almacen                                       | Sí          | Object          | CT_API_Publica / tbl_existencia |
-| `date_stored`                            |  Fecha en la que se capturó y guardó la información en colección                                       | Sí          | datetime         | CT_API_Publica / tbl_existencia |
-| `existenceId`                            |  Identificador único del documento del cual se extrajo la información                                      | Sí          |  ObjectId       | CT_API_Publica / tbl_existencia |
+| **Nombre**                            | **Nombre en tabla** | **Descripción**                                                    | **Requerido**         | **Formato de variable** | **Fuente**                  |
+| ------------------------------------- | ------------------------------------- | ------------------------------------------------------------------ | -------------------- | ----------------------- | --------------------------- |
+| `_id`                            | _id | Identificador único del documento                                      | Sí          | ObjectId          | CT_API_Publica / tbl_existenciasHistorial |
+| `existenceId`                            | existenciaId| Identificador único del documento del cual se extrajo la información                                      | Sí          |  ObjectId       | CT_API_Publica / tbl_existencias |
+| `productId`                            |codigo | Código identificador de producto                                       | Sí          | Texto / String          | CT_API_Publica / tbl_existencias |
+| `active`                            |activo|  Describe si el producto esta actualmente activo                                       | Sí          | Array         | CT_API_Publica / tbl_existencias |
+| `cost`                            |costo|  Cuántas unidades existen por almacen                                       | Sí          | Array         | DWH / CAT_ARTICULOS |
+| `units_stored`                            |almacenes|  Cuántas unidades existen por almacen                                       | Sí          | Array           | CT_API_Publica / tbl_existencias |
+| `date_stored`                            |fechaRegistro|  Fecha en la que se capturó y guardó la información en colección                                       | Sí          | datetime         | CT_API_Publica / tbl_existenciasHistorial |
+| `date_updae`                            |fechaUpdate|  Fecha en la que se actualizó la información del documento en la colección                                       | Sí          | datetime         | CT_API_Publica / tbl_existenciasHistorial |
+
--- stock_ingest.py
+++ stock_ingest.py
@@ -2,20 +2,16 @@ import os
 import pandas as pd
 import logging
 import datetime
-from data_loader import load_product_codes
+from data_loader import load_product_codes,get_query 
 from dotenv import load_dotenv
-from pymongo import MongoClient 
+from bson import BSON
+from pymongo import MongoClient , UpdateOne
 
 load_dotenv()
 
-logging.basicConfig(
-    filename='mongo_stats.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
 # -----------------------------------------------------------
 # -----------------------------------------------------------
+id_fields = ["_id","existenciaId","codigo"]
 
 example_date = datetime.date.today()
 example_doc = {   "_id":123,
@@ -27,12 +23,23 @@ example_client = MongoClient()
 
 Date = type( example_date )
 Document = type( example_doc )
-Documents = type ( [example_doc,example_doc] )
+Documents = type ( {example_doc["_id"]:example_doc} )
 Client = type ( example_client)
 
 # -----------------------------------------------------------
 # -----------------------------------------------------------
 
+
+
+# -----------------------------------------------------------
+# LOGS
+# -----------------------------------------------------------
+
+logging.basicConfig(
+    filename='mongo_stats.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 def log_collection_size(db, collection_name:str,num_inserted_docs:int=None):
     stats = db.command("collStats", collection_name)
@@ -54,8 +61,19 @@ def log_collection_size(db, collection_name:str,num_inserted_docs:int=None):
             f"totalSize={stats.get('totalSize', 0)} bytes"
         )
 
+# -----------------------------------------------------------
+# CONEXIÓN
+# -----------------------------------------------------------
 
-def connect_to_DB()-> Client:
+
+
+def connect_to_DB()-> MongoClient:
+    """
+    Docstring for connect_to_DB
+    
+    :return: Cliente de mongoDB
+    :rtype: Any
+    """
     conn_uri = os.getenv("MONGO_URI")
     mongo_db = os.getenv("MONGO_DB")
     existence_table = os.getenv("EXISTENCE_COLLECTION")
@@ -63,8 +81,13 @@ def connect_to_DB()-> Client:
 
     client = MongoClient(conn_uri)
     db = client[mongo_db]
+    return db
 
-def get_documents() -> tuple( Documents ,Documents ):
+# -----------------------------------------------------------
+# CONSULTA
+# -----------------------------------------------------------
+
+def get_documents() -> tuple[ Documents ,Documents ]:
     """
     Función que retorna conjunto de documentos extraídos de la colección de existencia
     
@@ -73,100 +96,196 @@ def get_documents() -> tuple( Documents ,Documents ):
     """
     db = connect_to_DB()
 
-    # Tabla de existencia
-    existence_table = os.getenv("EXISTENCE_COLLECTION")
-    filter_condition = {"almacenes.existencia": {"$gt": 0}}
-    fields = {"codigo":1,
-            "activo":1,
-            "almacenes":1,
-            "_id":1}
+    consult_info = {"EXISTENCE_COLLECTION":{"filters":{"almacenes.existencia": {"$gt": 0},
+                                            "fields":{"codigo":1,"activo":1,"almacenes":1,"_id":1} }},
+
+                    "EXISTENCE_HIST_COLLECTION":{"filters": {},
+                                                 "fields": {} }   }
     
-    collection = db[existence_table]
-    cursor = collection.find( filter_condition,
-                            fields
-                            )
-    existence_docs = list(cursor)
+    result_docs = []
 
-    # Tabla de historial
-    hist_table = os.getenv("EXISTENCE_HIST_COLLECTION")
+    for table_name, info in consult_info.items():
+        table = os.getenv(table_name)  
 
-    return existence_docs,hist_docs
+        filters = info.get("filters", {})
+        fields = info.get("fields", {})
 
-def rename_fields (doc:Document)->Document:
+        collection = db[table]
+        cursor = collection.find(filters, fields)
+        
 
-    rename_dict = { "_id":"existenceId",
-                    "codigo":"productId",
-                    "activo":"active",
-                    "almacenes":"inventory",
-                    }
+        doc_dict = {doc["_id"]: doc for doc in cursor}
+        if table_name=="EXISTENCE_HIST_COLLECTION":
+            doc_dict = {doc["existenceId"]: doc for doc in cursor}
 
+        result_docs.append(doc_dict)
 
+ 
+    return result_docs[0],result_docs[1]
 
-def delta_docs(new_doc: Document ,old_doc: Document )-> Document:
+def get_product_cost_dict()-> dict :
     """
-    Función que detecta si dos documentos son iguales o diferentes.
-    Regresa un documento con solo los campos actualizados.
-    """
-    delta_doc = []
-    for key in new_doc:
-        # Si la llave no existe en old_doc o el valor cambió, la agregamos
-        if key not in old_doc or new_doc[key] != old_doc[key]:
-            delta_doc[key] = new_doc[key]
-
-    return delta_doc
-
-def update_
-    today = datetime.date.today()
-    for d in docs:
-        d["date_stored"] = today
-
+    Función que consulta el datawarehouse para conseguir los costos de productos y los regresa
+    como diccionario.
     
-    db[hist_table].insert_many(insert_docs)
-    db[hist_table].update_many(update_docs)
+    :return: Regresa un diccionario donde las llaves son el código del producto (productId) y los valores el costo correspondiente
+    :rtype: dict
+    """
+    table = os.getenv("ART_TABLE_NAME")
+    art_col = os.getenv("ARTICLE_COLUMN")
+    art_cost = os.getenv("ARTICLE_COST")
 
-# -----------------------------------------------------------
-# CONEXIÓN
-# -----------------------------------------------------------
-
-
-
-# -----------------------------------------------------------
-# CONSULTA
-# -----------------------------------------------------------
-
-
-exist_docs ,hist_docs = get_documents()
-
-product_codes = load_product_codes()
-
-
-today = datetime.date.today()
-for d in docs:
-    d["date_stored"] = today
-
+    query = f""" SELECT {art_col},{art_cost} 
+                 FROM {table}
+            """
+    df = get_query(query)
+    cost_dict = df.set_index(art_col).to_dict(orient="index")
+    return cost_dict
 
 
 # -----------------------------------------------------------
 # FILTROS
 # -----------------------------------------------------------
 
+def delta_docs(new_doc: Document ,old_doc: Document )-> Document:
+    """
+    Función que detecta si dos documentos son iguales o diferentes.
+    Regresa un documento con solo los campos actualizados.
+    """
+    delta_doc = {}
+    for key in new_doc:
+        # Si la llave no existe en old_doc o el valor cambió, la agregamos
+        if ( key not in old_doc ) or ( new_doc[key] != old_doc[key] ):
+            delta_doc[key] = new_doc[key]
 
-update_docs =
-insert_docs =
+    return delta_doc
+
+
+def compare_doc_size(ref_doc:Document,update_doc:Document)->tuple[float,bool]:
+    """
+    Función que recibe el documento de referencia y de actualización y comparar sus tamaños combinados en MB con un umbral para
+    lograr saber si el documento de referencia puede llegar a actualizarse correctamente sin problema 
+    a la colección de historiales. 
+    
+    :param ref_doc: Documento de referencia, el documento que se quiere actualizar.
+    :type ref_doc: Document
+    :param update_doc: Documento con información de actualización.
+    :type update_doc: Document
+    :return: Resultados de comparación, el primer elemento es la diferencia de tamaños en MB, y el segundo un booleano representando si se puede actualizar el documento base o no
+    :rtype: tuple[float, bool]
+    """
+    threshold =  16 * (1024**2) # bytes
+    ref_bytes = BSON.encode(ref_doc)
+    update_bytes = BSON.encode(update_doc)
+
+    
+    ref_size = len(ref_bytes)
+    update_size = len(update_bytes)
+
+    diff_size = threshold  - (ref_size + update_size)
+
+    diff_size_MB = round ( diff_size / 1024**2 , 2 )
+    can_update = threshold > ref_size + update_size 
+    return diff_size_MB, can_update
+
 
 # -----------------------------------------------------------
 # INGESTA Y ACTUALIZACIÓN DE DOCS
 # -----------------------------------------------------------
 
+def update_table(updates:Documents):
+    """
+    Función que recibe los documentos que se quieren actualizar y los documentos que contienen solo la
+    información actualizada. Actualiza solo los cambios a la tabla de historial. 
+    
+    :param docs: Documentos referencia que se actualizaran
+    :type docs: Documents
+    :param update_info: Documentos que contienen información actualizada 
+    :type update_docs: Documents
+
+
+    """
+
+    today = datetime.date.today()
+    update_ops = []
+    
+    for u in updates:
+        information = {}
+        for field,value in u.items():
+            if field in id_fields:
+                continue
+            if isinstance(value, list):
+                information[field] = { "$each":value }
+            else:
+                information[field] = { "$each":[value] }
+
+        update_ops.append(
+        UpdateOne(
+            {"_id": u["_id"],"codigo": u["codigo"]}, # Filtros
+            {
+                "$push": information,
+                "$currentDate": {"fechaUpdate": True}
+            }
+        )
+    )
+        
+    db = connect_to_DB()
+    hist_table = os.getenv("EXISTENCE_HIST_COLLECTION")
+    db[hist_table].bulk_write(update_ops, ordered=False)
+
+    print(f"{len(updates)} documentos actualizados correctamente!")  
 
 
 
 
 
 # -----------------------------------------------------------
-# LOGS
+# MAIN PIPELINE
 # -----------------------------------------------------------
 
+def main():
+    db = connect_to_DB()
 
-num_inserted_docs = len(insert_docs)
-log_collection_size(db,hist_table,num_inserted_docs)
+    exist_docs ,hist_docs = get_documents()
+    product_codes = load_product_codes()
+    cost_dict = get_product_cost_dict()
+
+
+    update_docs = []
+    insert_docs = []
+    for existenceId,e in exist_docs.items():   
+        h = hist_docs[existenceId]
+        productId = e["codigo"]
+
+        # Comparación de información
+        old_doc = {
+                "codigo":h["codigo"],
+                "costo":h["costo"][-1],
+                "activo":h["activo"][-1],
+                "almacen":h["almacen"][-1] }
+        
+        new_doc = {
+                "codigo":productId,
+                "costo":cost_dict[productId],
+                "activo":e["activo"],
+                "almacen":e["almacen"] }
+        
+        updates = delta_docs(new_doc,old_doc)
+        
+        # Comparación de tamaño
+
+        size_diff,is_update = compare_doc_size(ref_doc=h,update_doc=updates)
+        
+        if is_update:
+            update_docs.append(updates)
+        else:
+            insert_docs.append(updates)
+
+
+    hist_table = os.getenv("EXISTENCE_HIST_COLLECTION")
+    update_table(update_docs)
+    db[hist_table].insert_many(insert_docs)
+
+
+    num_inserted_docs = len(insert_docs)
+    log_collection_size(db,hist_table,num_inserted_docs)
