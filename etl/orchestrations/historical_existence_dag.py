9dc79add89b124e417007e50e38ffa679ffda83f
fdcir3tt <fd.cirett@gmail.com>
2026-05-06 11:56:21 -0700

    DAG de ingesta de datos historiales de existencias

---
+++ etl/orchestrations/historical_existence_dag.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+
+from airflow.operators.python import PythonOperator
+from airflow.operators.python import ShortCircuitOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+from pipelines.historical_existence.extract import run_extract
+from pipelines.historical_existence.transform import run_transform
+from pipelines.historical_existence.load import run_load
+
+#  Configuración del DAG
+default_args = {
+    "owner": "Federico",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "email_on_failure": False,
+}
+
+def should_continue(**context):
+    up_to_date = context["ti"].xcom_pull(task_ids="extract",key="up_to_date")
+
+    
+    return up_to_date  
+
+
+with DAG(
+    dag_id="historical_existence_pipeline",
+    default_args=default_args,
+    description="Pipeline de ingesta de existencias a tabla de historial de existencias ",
+    schedule_interval="1 3 * * *",  # Diario a las 6am MST -7
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["external"],
+) as dag:
+
+    
+
+    extract_task = PythonOperator(
+        task_id="extract_existence_docs",
+        python_callable=run_extract,
+    )
+    check = ShortCircuitOperator(
+        task_id="check_if_collection_up_to_date",
+        python_callable=should_continue,
+    )
+    transform_task = PythonOperator(
+        task_id="make_new_docs",
+        python_callable=run_transform,
+    )
+
+    load_task = PythonOperator(
+        task_id="load_docs",
+        python_callable=run_load,
+    )
+
+    trigger_inventory_dag = TriggerDagRunOperator(
+        task_id="trigger_inventory_dag",
+        trigger_dag_id="inventory_pipeline",
+    )
+    extract_task >> check >> transform_task >> load_task >> trigger_inventory_dag
---
+++ etl/pipelines/historical_existence/load.py
@@ -0,0 +1,29 @@
+import os
+import pymongo
+
+from bson import ObjectId
+from typing import Any
+from dotenv import load_dotenv
+from common.db import connect_to_mongo_db
+from common.paths import ENV_DIR
+
+env_path = ENV_DIR /".env"
+load_dotenv(env_path)
+hist_mongo_uri = os.getenv("TRACE_MONGO_URI")
+hist_db_name = os.getenv("TRACE_EXISTENCE_DB_NAME")
+trace_existence_collection = os.getenv("TRACE_EXISTENCE_COLLECTION")
+batch_size = 500 
+table_name="tbl_existenciasHistorial"
+
+def load(documents:list[dict[str,Any]],database:pymongo.database.Database,collection_name:str,batch_size:int): 
+    for doc in documents:
+        doc["productoReferencia.existenciaId"] = ObjectId(doc.get("productoReferencia.existenciaId"))
+    for i in range(0, len(documents), batch_size):
+        chunk = documents[i:i+batch_size]
+        database[collection_name].insert_many(chunk)
+
+def run_load(**context):
+    insert_documents = context["ti"].xcom_pull(task_ids="make_new_docs", key="docs_to_insert")
+    hist_database = connect_to_mongo_db(hist_mongo_uri,hist_db_name)
+    load(insert_documents,hist_database,table_name,batch_size)
+    
