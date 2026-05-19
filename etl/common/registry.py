e9fddb502cfbca35bc2ce594c281c9e39f4a9412
fdcir3tt <fd.cirett@gmail.com>
2026-05-19 10:54:41 -0700

    Registro de funciones por nombre específicado

--- etl/common/registry.py
+++ etl/common/registry.py
@@ -2,6 +2,6 @@ FUNCTION_REGISTRY = {}
 
 def register(name=None):
     def wrapper(func):
-        FUNCTION_REGISTRY[name or func.__name__] = func
+        FUNCTION_REGISTRY[f"{name}.{func.__name__}"] = func
         return func
     return wrapper
