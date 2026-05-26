a5cdffe1d409a9f300668cb5df309eeb3d49753a
fdcir3tt <fd.cirett@gmail.com>
2026-05-26 09:36:25 -0700

    Script para actualizar permisos de usuario de airflow

---
+++ etl/fix_permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo chown -R 50000:0 ./orchestrations
+sudo chmod -R 755 ./orchestrations
+
+sudo chown -R 50000:0 ./common
+sudo chmod -R 755 ./common
+
+sudo chown -R 50000:0 ./pipelines
+sudo chmod -R 755 ./pipelines
